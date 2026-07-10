f82c03320bc51823f76e7b53e327f619e17a3d8f
Sajal Gupta <sajal2005gupta@gmail.com>
2026-07-10 23:21:45 +0530

    cpu: implement ADD, OR, XOR instructions

--- src/cpu/cpu.rs
+++ src/cpu/cpu.rs
@@ -10,6 +10,9 @@ enum Instruction {
   ADC(ArithmeticTarget),
   SUB(ArithmeticTarget),
   SBC(ArithmeticTarget),
+  AND(ArithmeticTarget),
+  OR(ArithmeticTarget),
+  XOR(ArithmeticTarget),
 }
 
 enum ArithmeticTarget {
@@ -52,6 +55,8 @@ impl Cpu {
 
     fn add_carry(&mut self, val: u8) -> u8 {
         let carry = if self.registers.f.carry { 1 } else { 0 };
+        // BUG: if val == 0xFF and carry is set, this will overflow
+        //      same in the sub_carry method
         let (new_val, overflow) = self.registers.a.overflowing_add(val + carry);
         self.registers.f.zero = new_val == 0;
         self.registers.f.subtract = false;
@@ -76,6 +81,33 @@ impl Cpu {
         self.registers.f.subtract = true;
         self.registers.f.carry = borrow;
         self.registers.f.half_carry = (self.registers.a & 0xF) < ((val & 0xF) + carry);
+        new_val
+    }
+    
+    fn and(&mut self, val: u8) -> u8 {
+        let new_val = self.registers.a & val;
+        self.registers.f.zero = new_val == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = false;
+        self.registers.f.half_carry = true;
+        new_val
+    }
+
+    fn or(&mut self, val: u8) -> u8 {
+        let new_val = self.registers.a | val;
+        self.registers.f.zero = new_val == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = false;
+        self.registers.f.half_carry = false;
+        new_val
+    }
+
+    fn xor(&mut self, val: u8) -> u8 {
+        let new_val = self.registers.a ^ val;
+        self.registers.f.zero = new_val == 0;
+        self.registers.f.subtract = false;
+        self.registers.f.carry = false;
+        self.registers.f.half_carry = false;
         new_val
     }
 
@@ -130,6 +162,41 @@ impl Cpu {
                     ArithmeticTarget::E => { self.registers.a = self.sub_carry(self.registers.e); },
                     ArithmeticTarget::H => { self.registers.a = self.sub_carry(self.registers.h); },
                     ArithmeticTarget::L => { self.registers.a = self.sub_carry(self.registers.l); },
+                }
+            },
+
+            Instruction::AND(target) => {
+                match target {
+                    ArithmeticTarget::A => { self.registers.a = self.and(self.registers.a); },
+                    ArithmeticTarget::B => { self.registers.a = self.and(self.registers.b); },
+                    ArithmeticTarget::C => { self.registers.a = self.and(self.registers.c); },
+                    ArithmeticTarget::D => { self.registers.a = self.and(self.registers.d); },
+                    ArithmeticTarget::E => { self.registers.a = self.and(self.registers.e); },
+                    ArithmeticTarget::H => { self.registers.a = self.and(self.registers.h); },
+                    ArithmeticTarget::L => { self.registers.a = self.and(self.registers.l); },
+                }
+            },
+            Instruction::OR(target) => {
+                match target {
+                    ArithmeticTarget::A => { self.registers.a = self.or(self.registers.a); },
+                    ArithmeticTarget::B => { self.registers.a = self.or(self.registers.b); },
+                    ArithmeticTarget::C => { self.registers.a = self.or(self.registers.c); },
+                    ArithmeticTarget::D => { self.registers.a = self.or(self.registers.d); },
+                    ArithmeticTarget::E => { self.registers.a = self.or(self.registers.e); },
+                    ArithmeticTarget::H => { self.registers.a = self.or(self.registers.h); },
+                    ArithmeticTarget::L => { self.registers.a = self.or(self.registers.l); },
+                }
+            },
+            
+            Instruction::XOR(target) => {
+                match target {
+                    ArithmeticTarget::A => { self.registers.a = self.xor(self.registers.a); },
+                    ArithmeticTarget::B => { self.registers.a = self.xor(self.registers.b); },
+                    ArithmeticTarget::C => { self.registers.a = self.xor(self.registers.c); },
+                    ArithmeticTarget::D => { self.registers.a = self.xor(self.registers.d); },
+                    ArithmeticTarget::E => { self.registers.a = self.xor(self.registers.e); },
+                    ArithmeticTarget::H => { self.registers.a = self.xor(self.registers.h); },
+                    ArithmeticTarget::L => { self.registers.a = self.xor(self.registers.l); },
                 }
             },
             _ => {},
